26b182dc19e1f2cf6bd0a06d2d6a22d9af35d30c
jbmdevelopment <jbarreramartinez100@student.hbuhsd.edu>
2024-10-07 10:58:09 -0700

    aaa

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main
 {
   public static String LastFirstN(String s1, String s2, int n) {
-    String lastS1 = s1.substring(n); // fix this later
+    String lastS1 = s1.substring(s1.length() - n); // fix this later
     String firstS2 = s2.substring(0, n);
     String concatted = lastS1 + firstS2;
     return concatted;
@@ -9,7 +9,7 @@ public class Main
   public static void main(String[] args)
   {
     String testy = LastFirstN("cream", "butter", 3);
-    String testy1 = LastFirstN("superman", "pee", 3);
+    String testy1 = LastFirstN("superman", "peee", 3);
     System.out.println(testy);
     System.out.println(testy1);
     // write your code here
